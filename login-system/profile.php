0b53c7699d566e769bc145f7a053a860345b3064
tilalx <tilo.alexander@gmx.de>
2022-10-03 10:55:01 +0200

    ccommit 9

--- login-system/profile.php
+++ login-system/profile.php
@@ -6,10 +6,10 @@ if (!isset($_SESSION['loggedin'])) {
 	header('Location: index.html');
 	exit;
 }
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
+$DATABASE_HOST = '192.168.178.39';
+$DATABASE_USER = 'climb';
 $DATABASE_PASS = '';
-$DATABASE_NAME = 'phplogin';
+$DATABASE_NAME = 'climbing-manager';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
